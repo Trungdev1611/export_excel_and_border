c4e65c1ea929e4769499e9c617b5f7d1dfa093a8
Trungdev <trungpv@winds.vn>
2022-12-11 10:54:06 +0700

    component reuse

--- src/App.js
+++ src/App.js
@@ -1,68 +1,36 @@
 import "./styles.css";
-import ExcelJS from "exceljs";
-import FileSaver from "file-saver";
+
+import ExportExcel from "./exportExcel";
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function App() {
-  async function exportDataExcel() {
-    //tạo mới 1 workbook
-    const workbook = new ExcelJS.Workbook();
-    //tạo mới 1 worksheet tên Mysheet, màu tab sheet, chiều cao hàng
-    const sheet = workbook.addWorksheet("MySheet");
-
-    //đóng băng hàng đầu
-    // sheet.views = [
-    //   {state: 'frozen'}
-    // ];
-
-    //thêm cột cho sheet
-    sheet.columns = [
-      { header: "Id", key: "id", width: 10 },
-      { header: "Name", key: "name", width: 12 },
-      { header: "dob.", key: "dob", width: 20 }
-    ];
-
-    //thêm các hàng cho sheet (có rất nhiều các thêm xem trong npm docs)
-    //C1
-    // sheet.addRow({id: 1, name: 'John Doe', dob: new Date(1970,1,1)});
-    // sheet.addRow({id: 2, name: 'Jane Doe', dob: new Date(1965,1,7)});
-
-    //C2 - rows các object
-    const rows = [
-      { id: 1, name: "Barbara", dob: 111111111 },
-      { id: 2, name: "Barbara", dob: 62222222222 },
-      { id: 3, name: "Barbara", dob: 3333333333 }
-    ];
-    sheet.addRows(rows);
-
-    //lap qua tat ca nhung o hien hanh cua cot
-    for (let i = 1; i <= rows.length; i++) {
-      sheet.getColumn(i).eachCell(function (cell, rowNumber) {
-        cell.style.font = { name: "Comic Sans MS" };
-        cell.style.border = {
-          top: { style: "thin" },
-          left: { style: "thin" },
-          bottom: { style: "thin" },
-          right: { style: "thin" }
-        };
-      });
+  const [dataRowExcel, setDataRowExcel] = useState([]);
+  useEffect(() => {
+    async function getDataExcel() {
+      try {
+        let res = await axios.get("https://retoolapi.dev/ZfWdYC/export_excel");
+        let data = res.data;
+        console.log("data::", data);
+        let datamap =
+          data.length > 0 &&
+          data.map((ele) => {
+            return { ...ele, stt: ele.id };
+          });
+        setDataRowExcel(datamap);
+      } catch (error) {
+        throw error;
+      }
     }
-
-    //dowload file
-    const buffer = await workbook.xlsx.writeBuffer();
-    saveFileDowload(buffer);
-
-    // }
-  }
-
-  function saveFileDowload(data) {
-    let blob = new Blob([data], {
-      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;"
-    });
-    FileSaver.saveAs(blob, "Myfile.xlsx");
-  }
+    getDataExcel();
+  }, []);
 
   return (
     <div className="App">
-      <button onClick={exportDataExcel}>Export Excel</button>
+      <ExportExcel
+        dataRowExcel={dataRowExcel}
+        sheetname="dataExport"
+        filename={"exportExcel.xlsx"}
+      />
     </div>
   );
 }
---
+++ src/constant.js
@@ -0,0 +1,11 @@
+export const columnsExcel = [
+  { header: "STT", key: "stt", width: 10 },
+  { header: "Mã hàng", key: "catalog", width: 20 },
+  { header: "Tên hàng", key: "productName", width: 20 },
+  { header: "ĐVT", key: "unit", width: 10 },
+  { header: "Số lượng", key: "count", width: 15 },
+  { header: "Phân loại", key: "classify", width: 15 },
+  { header: "Giá khuyến mãi", key: "buyprice", width: 25 },
+  { header: "Giá niêm yết", key: "inputPrice", width: 25 },
+  { header: "Thành tiền", key: "totalMomney", width: 25 },
+];
